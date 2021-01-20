608992e1ad318f2ea18b19cfc2e9524e2cd823a4
Nazorm <nnadoziechinazor@gmail.com>
2021-01-20 01:49:43 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{ useState, useEffect} from 'react';
 
 function App() {
+const [location, setLocation]= useState('')
+const [destination, setDestination] = useState('')
+
+  const handleLocationChange=(e)=>{
+const {value} = e.target
+setLocation(value)
+
+  }
+  const handleDestinationChange=(e)=>{
+    const {value} = e.target
+    setDestination(value)
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    if(!location || !destination){
+      alert('enter location or destination')
+    }else{
+      alert(`${location} to ${destination}`)
+    }
+  }
   return (
     <div className="App">
-      <h1>one</h1>
+      <form onSubmit={handleSubmit}>
+      <input
+      name='location'
+      onChange={handleLocationChange}
+      value={location}
+      placeholder='enter location'
+      />
+      <br/>
+      <input
+      name='destination'
+      onChange={handleDestinationChange}
+      value={destination}
+      placeholder='enter destination'
+      />
+      <br/>
+      
+      <button>Enter</button>
+      </form>
+     
       
     </div>
   );
