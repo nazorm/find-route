85cd6135052e2aa00214acc42e7b964e72af27eb
nazorm <nnadoziechinazor@gmail.com>
2021-01-23 05:54:42 -0800

    App

--- src/App.js
+++ src/App.js
@@ -1,51 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-import React,{ useState, useEffect} from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Container, Button } from "react-bootstrap";
 
 function App() {
-const [location, setLocation]= useState('')
-const [destination, setDestination] = useState('')
+  const [location, setLocation] = useState("");
+  const [destination, setDestination] = useState("");
 
-  const handleLocationChange=(e)=>{
-const {value} = e.target
-setLocation(value)
-
-  }
-  const handleDestinationChange=(e)=>{
-    const {value} = e.target
-    setDestination(value)
-  }
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-    if(!location || !destination){
-      alert('enter location or destination')
-    }else{
-      alert(`${location} to ${destination}`)
+  const handleLocationChange = (e) => {
+    const { value } = e.target;
+    setLocation(value);
+  };
+  const handleDestinationChange = (e) => {
+    const { value } = e.target;
+    setDestination(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!location || !destination) {
+      alert("enter location or destination");
+    } else {
+      alert(`${location} to ${destination}`);
     }
-  }
+  };
   return (
-    <div className="App">
+    <Container>
       <form onSubmit={handleSubmit}>
-      <input
-      name='location'
-      onChange={handleLocationChange}
-      value={location}
-      placeholder='enter location'
-      />
-      <br/>
-      <input
-      name='destination'
-      onChange={handleDestinationChange}
-      value={destination}
-      placeholder='enter destination'
-      />
-      <br/>
-      
-      <button>Enter</button>
+        <input
+          name="location"
+          onChange={handleLocationChange}
+          value={location}
+          placeholder="enter location"
+        />
+        <br />
+        <input
+          name="destination"
+          onChange={handleDestinationChange}
+          value={destination}
+          placeholder="enter destination"
+        />
+        <br />
+        <Button>Enter</Button>
       </form>
-     
-      
-    </div>
+    </Container>
   );
 }
 
