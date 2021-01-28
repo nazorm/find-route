21cad42a538c1cb7b29773cf5ecb5ea1f1ebcfdb
nazorm <nnadoziechinazor@gmail.com>
2021-01-28 21:56:24 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { Container, Button } from "react-bootstrap";
+import { Container, Alert, Button, Form } from "react-bootstrap";
 
 function App() {
   const [location, setLocation] = useState("");
   const [destination, setDestination] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleLocationChange = (e) => {
     const { value } = e.target;
@@ -25,23 +26,37 @@ function App() {
   };
   return (
     <Container>
-      <form onSubmit={handleSubmit}>
-        <input
-          name="location"
-          onChange={handleLocationChange}
-          value={location}
-          placeholder="enter location"
-        />
-        <br />
-        <input
-          name="destination"
-          onChange={handleDestinationChange}
-          value={destination}
-          placeholder="enter destination"
-        />
-        <br />
-        <Button>Enter</Button>
-      </form>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId={location}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={handleLocationChange}
+            placeholder="Enter Your Location"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your location without your permission.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId={destination}>
+          <Form.Label>Destination</Form.Label>
+          <Form.Control
+            type="text"
+            onChange={handleDestinationChange}
+            placeholder="Enter your Destination"
+          />
+          <Form.Text className="text-muted">
+            We'll never share your destination without your permission.
+          </Form.Text>
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Enter
+        </Button>
+      </Form>
+
+      <div className="direction-container"></div>
     </Container>
   );
 }
