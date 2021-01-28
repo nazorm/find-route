a14036a5ce300c535c9ca5b744f5e8fbea02787d
nazorm <nnadoziechinazor@gmail.com>
2021-01-29 00:09:54 +0100

    Directions

---
+++ src/components/Direction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Direction=()=>{
+    return(
+        <div>
+            <div className="modeOfTransit"></div>
+            <div className="direction-container"> 
+            <h4>direction goes here</h4>
+            </div>
+          
+        
+      </div>
+    )
+}
+
+
+export default Direction
