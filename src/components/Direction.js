c75a5e5489f677b6fd60e93d176ad2f8899b5180
nazorm <nnadoziechinazor@gmail.com>
2021-01-30 20:36:15 +0100

    Direction

--- src/components/Direction.js
+++ src/components/Direction.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import '../App.css'
 
-const Direction=()=>{
+const Direction=(props)=>{
+    const directions = props.directionStats.map((d, index)=>{
+        return(
+            <div className='directions-container' key={index}>
+                <div className="modeOfTransit">
+                <span>Summary: {d.summary}</span>
+                <span>Distance: {d.legs[0].distance.text}</span>
+                <span>Duration: {d.legs[0].duration.text}</span><br/>
+                <span>Start: {d.legs[0].start_address}</span>
+                <span>End: {d.legs[0].end_address}</span>
+                </div>
+                <div className="direction-container"> 
+
+                </div>
+              
+            <div className="stats"></div>
+          </div>
+        )
+    });
+
+
     return(
         <div>
-            <div className="modeOfTransit"></div>
-            <div className="direction-container"> 
-            <h4>direction goes here</h4>
-            </div>
-          
-        
-      </div>
+            {directions}
+        </div>
     )
+ 
 }
 
 
